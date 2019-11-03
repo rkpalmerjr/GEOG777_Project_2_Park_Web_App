0006d8cd26c8373aacaf75f9e8a83a204471a8bb
Kevin Palmer <rkpalmerjr@gmail.com>
2019-11-02 23:31:32 -0400

    Lots of stuff

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/* Kevin Palmer, UW-Madison, GEOG777, Fall 2019, Project 2 */
+
+// Define basemap tilesets
+var light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHNteXRoMiIsImEiOiJjaXNmNGV0bGcwMG56MnludnhyN3Y5OHN4In0.xsZgj8hsNPzjb91F31-rYA', {
+        maxZoom: 21,
+        id: 'mapbox.streets',
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+    }),
+    dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHNteXRoMiIsImEiOiJjaXNmNGV0bGcwMG56MnludnhyN3Y5OHN4In0.xsZgj8hsNPzjb91F31-rYA', {
+        maxZoom: 21,
+        id: 'mapbox.dark',
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+    }),
+    streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHNteXRoMiIsImEiOiJjaXNmNGV0bGcwMG56MnludnhyN3Y5OHN4In0.xsZgj8hsNPzjb91F31-rYA', {
+        maxZoom: 21,
+        id: 'mapbox.streets',
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+    }),
+    imagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        maxZoom: 21,
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+// Create the map
+var map = L.map('map', {
+    center: [38.872778, -77.0075],
+    zoom: 16,
+    minZoom: 12,
+    maxZoom: 21,
+    layers: [imagery]
+});
+
+// Create basemap tileset layers
+var baseMaps = {
+    "Light Gray": light,
+    "Dark Gray": dark,
+    "OSM Streets": streets,
+    "Esri Imagery": imagery
+};
+
+// Add layers control to the map
+var layerControl = L.control.layers(baseMaps);
+layerControl.addTo(map);
+$('<p class = "controlHeader">Basemap Tilesets</p>').insertBefore('div.leaflet-control-layers-base');
+
+// Add locator
+L.control.locate().addTo(map);
